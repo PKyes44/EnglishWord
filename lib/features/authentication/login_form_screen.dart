f06793397b3c8d35a32e4eb2bf4c2b70e71fc631
PKyes44 <rugate23@gmail.com>
2023-11-29 15:20:41 +0900

    20231129-04

--- lib/features/authentication/login_form_screen.dart
+++ lib/features/authentication/login_form_screen.dart
@@ -1,8 +1,10 @@
+import 'package:english_quiz/features/menu/menu_screen.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:english_quiz/constants/gaps.dart';
 import 'package:english_quiz/constants/sizes.dart';
 import 'package:english_quiz/features/widgets/form_button.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginFormScreen extends StatefulWidget {
   const LoginFormScreen({super.key});
@@ -16,19 +18,50 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
 
   Map<String, String> formData = {};
 
-  List<Map<String, String>> userData = [];
+  Map<String, String> userData = {};
+  late SharedPreferences _prefs; // SharedPreferences 객체
+
+  String nickname = '';
 
   void _onSubmitTap() {
     if (_formKey.currentState != null) {
       if (_formKey.currentState!.validate()) {
         _formKey.currentState!.save();
+        _saveData();
+        Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(
+                builder: (BuildContext context) => MenuScreen(
+                      nickname: nickname,
+                    )),
+            (route) => false);
       }
     }
+  }
+
+  // 데이터를 저장하는 함수
+  Future<void> _saveData() async {
+    _prefs.setString('nickname', nickname);
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text('저장완료 : Words')), // 저장 완료 메시지 출력
+    );
   }
 
   @override
   void initState() {
     var readData = readUserData();
+    readData.then((value) {
+      var tempData = value;
+      var dataList = [];
+      for (var data in tempData.values) {
+        dataList.add(data);
+      }
+      for (var data in dataList) {
+        userData[data['nickname']] = data['password'];
+        setState(() {});
+      }
+      print('userData : $userData');
+    });
     // TODO: implement initState
     super.initState();
   }
@@ -36,9 +69,28 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
   Future<Map<Object?, Object?>> readUserData() async {
     final reference = FirebaseDatabase.instance.ref();
     DataSnapshot snapshot = await reference.child('user/').get();
-    final readData = snapshot.value as Map<Object?, Object?>;
+    return snapshot.value as Map<Object?, Object?>;
+  }
 
-    return readData;
+  bool matchNickname(String nickname) {
+    if (!userData.containsKey(nickname)) {
+      return false;
+    }
+    return true;
+  }
+
+  bool matchPassword(String password) {
+    if (!userData.containsValue(password)) {
+      return false;
+    }
+    return true;
+  }
+
+  bool matchData(String password) {
+    if (userData[nickname] == password) {
+      return true;
+    }
+    return false;
   }
 
   @override
@@ -74,6 +126,10 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
                   if (value != null && value.isEmpty) {
                     return "Plase write your nickname";
                   }
+                  if (!matchNickname(value.toString())) {
+                    return "Wrong nickname";
+                  }
+                  nickname = value.toString();
                   return null;
                 },
                 onSaved: (newValue) {
@@ -100,6 +156,10 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
                 validator: (value) {
                   if (value != null && value.isEmpty) {
                     return "Plase write your password";
+                  }
+                  if (!matchPassword(value.toString()) ||
+                      !matchData(value.toString())) {
+                    return "Wrong password";
                   }
                   return null;
                 },
--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:english_quiz/features/questions/question_list_screen.dart';
 import 'package:english_quiz/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'features/authentication/login_screen.dart';
 
@@ -16,8 +17,41 @@ void main() async {
   runApp(const EnglishQuizApp());
 }
 
-class EnglishQuizApp extends StatelessWidget {
+// ignore: must_be_immutable
+class EnglishQuizApp extends StatefulWidget {
   const EnglishQuizApp({super.key});
+
+  @override
+  State<EnglishQuizApp> createState() => _EnglishQuizAppState();
+}
+
+class _EnglishQuizAppState extends State<EnglishQuizApp> {
+  String nickname = '';
+  // late SharedPreferences _prefs;
+  @override
+  void initState() {
+    // init();
+    // TODO: implement initState
+    super.initState();
+  }
+  var data;
+
+  // void init() async {}
+
+  // 데이터를 로드하는 함수
+  Future<bool> checkLoginLog() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final myData = prefs.getString('nickname'); // 'myData' 키에 저장된 데이터 로드
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('로드완료 : $myData')), // 로드 완료 메시지와 함께 데이터 출력
+    );
+
+    if (myData == null) {
+      return false;
+    }
+    nickname = myData.toString();
+    return true;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,7 +71,9 @@ class EnglishQuizApp extends StatelessWidget {
         scaffoldBackgroundColor: Colors.white,
         primaryColor: const Color(0xffFF9F40),
       ),
-      home: const SignUpScreen(),
+      home: 
+          ? MenuScreen(nickname: nickname)
+          : const SignUpScreen(),
     );
   }
 }
